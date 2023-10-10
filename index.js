cef08f97ce1b6b901b7a663f82da8d5f6b0ab458
Mitali-goyal <60004351+Mitali-goyal@users.noreply.github.com>
2023-10-10 23:24:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ dotenv.config();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: "https://travel-map-backend-pi.vercel.app"
+}));
 
 mongoose
   .connect(process.env.MONGO_URL, {
