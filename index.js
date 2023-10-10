db8b43dc668282dfe2e1a22bf3c777850eed1a7b
Mitali-goyal <60004351+Mitali-goyal@users.noreply.github.com>
2023-10-10 23:55:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
-
+//allowed everyone to access 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
